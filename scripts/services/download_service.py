c1e01529fecdad620c9f8e802de80d111a4a38e2
Tiago Cambará <tiago.cambara@parceiros.nos.pt>
2024-12-12 20:18:53 -0300

    fix(sc-222): solve the issue when tries to download all the images at the same time

--- scripts/services/download_service.py
+++ scripts/services/download_service.py
@@ -1,21 +1,35 @@
 import os
+import time
 
 from concurrent.futures import ThreadPoolExecutor
 from decorators.singleton_decorator import singleton
 from infra.download import dowload_image
 from repositories.pokemon_repository import PokemonRepository
+from models.pokemon_model import PokemonModel
 
 @singleton
 class DownloadService:
     def __init__(self, pokemonRepository: PokemonRepository):
         self.pokemonRepository = pokemonRepository
     
-    def downloadPokemonImages(self, host:str, download_dir:str) -> None:
+    def downloadPokemonImages(self, host:str, download_dir:str, batch_size: int = 10, delay: float = 10.0) -> None:
         pokemons = self.pokemonRepository.getPokemons()
+        totalList = len(pokemons)
+        print("Started")
+        for i in range(0, totalList, batch_size):
+            batch_pokemons = pokemons[i:i+batch_size]
+            print(f"Started to download the batch {i // batch_size + 1}")
 
+            #Execute
+            self._download(batch_pokemons, host, download_dir)
+
+            if i + batch_size < totalList:
+                time.sleep(delay)
+        print("Finished")
+
+    def _download(self, pokemons: list[PokemonModel], host:str, download_dir:str, ) -> None:
         os.makedirs(download_dir, exist_ok=True)
 
-        print("Started to Download")
         with ThreadPoolExecutor() as executor:
             futures = []
             for pokemon in pokemons:
@@ -26,5 +40,3 @@ class DownloadService:
 
             for future in futures:
                 future.result()
-            print("Finished to Download")
-
